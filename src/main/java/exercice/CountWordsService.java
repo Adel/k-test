c74fdbdcb96fd6bf012cc21429a0192e40e4cceb
Adel <adel.elia@gmail.com>
2018-05-08 16:45:52 +0200

    working solution

--- src/main/java/exercice/CountWordsService.java
+++ src/main/java/exercice/CountWordsService.java
@@ -1,7 +1,12 @@
 package exercice;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class CountWordsService {
 
@@ -16,12 +21,42 @@ public class CountWordsService {
         }
     }
 
+    public static Stream<String> readFile(String filename) {
+        try {
+            return Files.lines(Paths.get(filename));
+        } catch (IOException e) {
+            throw new RuntimeException("error reading filename " + filename, e);
+        }
+    }
 
 
+    public void countWords(String inputFileName, String outputFileName) {
+        try (Stream<String> stream = readFile(inputFileName);
+             PrintWriter printWriter = getPrintWriter(outputFileName)) {
+            stream.forEach(this::processLine);
+            words.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                    .map(e -> new StringBuilder(e.getKey()).append("\t").append(e.getValue()).toString())
+                    .forEach(printWriter::println);
+        }
+    }
 
+    public static PrintWriter getPrintWriter(String filename) {
+        try {
+            return new PrintWriter(Files.newBufferedWriter(Paths.get(filename)));
+        } catch (IOException e) {
+            throw new RuntimeException("cannot write to file " + filename, e);
+        }
+    }
 
 
     public Integer getWordCount(String word) {
         return words.getOrDefault(word, 0);
+    }
+
+    public static void main(String[] args) {
+        CountWordsService service = new CountWordsService();
+        service.countWords("input.txt", "output.txt");
     }
 }
