78a161232dc2b3ce745995bc5b0c31edff4e48f8
Adel <adel.elia@gmail.com>
2018-05-08 16:58:48 +0200

    some changes

--- src/main/java/exercice/CountWordsService.java
+++ src/main/java/exercice/CountWordsService.java
@@ -44,7 +44,7 @@ public class CountWordsService {
                     .map(e -> new StringBuilder(e.getKey()).append("\t").append(e.getValue()).toString())
                     .forEach(printWriter::println);
             if (printWriter.checkError()) {
-                throw new RuntimeException("unknown error");
+                throw new RuntimeException("unknown error, cannot write to file");
             }
         }
     }
@@ -63,7 +63,12 @@ public class CountWordsService {
     }
 
     public static void main(String[] args) {
+        String input = "input.txt";
+        String output = "output.txt";
+        if (args.length == 1) {
+            input = args[0];
+        }
         CountWordsService service = new CountWordsService();
-        service.countWords("input.txt", "output.txt");
+        service.countWords(input, output);
     }
 }
